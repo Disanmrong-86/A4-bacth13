47e65c623c465d58c23f6ebdb42f4d8ca087b906
Disanmrong <mrongdisan86@gmail.com>
2026-02-26 17:18:27 +0600

    commit

--- script/script.js
+++ script/script.js
@@ -1,39 +1,24 @@
-// let interviewList = [];
-// let rejectList = [];
-let currentStatus = 'all';
+let currentStatus = "all";
 const tabActive = ["bg-blue-700","border-blue-800","text-white"];
 const tabInactive = ["bg-transparent","text-black","border-black"]
 
-
 let totalCount = document.getElementById('total-count')
-let jobCount = document.getElementById('job-count')
 let interviewCount = document.getElementById('interview-count')
 let rejectCount = document.getElementById('reject-count')
+const available = document.getElementById('job-count')
 
 
-const mainSection = document.querySelector('main');
-const cardSection = document.getElementById('allCards');
-
-const filterSection = document.getElementById('filter-section')
-
-
-// const allFilterBtn = document.getElementById('all-filter-btn');
-// const interviewFilterBtn = document.getElementById('interview-filter-btn');
-// const rejectFilterBtn = document.getElementById('reject-filter-btn');
+const allContainer = document.getElementById('all-container')
+const interviewContainer = document.getElementById('interview-container')
+const rejectContainer = document.getElementById('rejected-container')
+const emptyState = document.getElementById('filter-section');
 
 
-function calculation(){
-    totalCount.innerText = cardSection.children.length;
-    interviewCount.innerText = interviewList.length;
-    rejectCount.innerText = rejectList.length;
-    jobCount.innerText = totalCount.innerText ;  
-}
-calculation()
  
 
 function btnSwap(tab) {
      const tabs = ['all','interview','reject']
-
+    currentStatus = tab;
      for(const t of tabs){
        const tabName =  document.getElementById("tab-" + t)
        if(t === tab){
@@ -45,5 +30,85 @@ function btnSwap(tab) {
         tabName.classList.remove ( ... tabActive)
        }
      }
+
+     const pages = [allContainer,interviewContainer,rejectContainer];
+     for(const section of pages){
+        section.classList.add('hidden')
+     }
+
+
+emptyState.classList.add('hidden')
+
+     if(tab === 'all'){
+        allContainer.classList.remove('hidden')
+        if(allContainer.children.length < 1){
+            emptyState.classList.remove('hidden')
+        }
+     }
+     else if (tab === 'interview'){
+        interviewContainer.classList.remove('hidden')
+        if(interviewContainer.children.length < 1){
+            emptyState.classList.remove('hidden')
+        }
+     }
+     else{
+        rejectContainer.classList.remove('hidden')
+        if(rejectContainer.children.length < 1){
+            emptyState.classList.remove('hidden')
+        }
+     }
+     updateState();
 }
-btnSwap(currentStatus)
+btnSwap(currentStatus);
+
+totalCount.innerText = allContainer.children.length;
+// interviewCount.innerText = interviewContainer.children.length
+
+document.getElementById('jobs-container').addEventListener('click',function (event){
+    const clickedElement = event.target;
+    const card = clickedElement.closest('.card');
+    const status = card.querySelector('.status');
+    const parent = card.parentNode;
+    console.log(card)
+    
+    if(clickedElement.classList.contains('interview')){
+        status.innerText = "Interviewed";
+        interviewContainer.appendChild(card);
+     
+    }
+    if(clickedElement.classList.contains('reject')){
+        status.innerText = "Rejected";
+        rejectContainer.appendChild(card);
+      
+    }
+    if(clickedElement.classList.contains('delete')){
+        parent.removeChild(card)
+    }
+    updateState();
+})
+
+
+function updateState(){
+    // totalCount.innerText = allContainer.children.length
+    // interviewCount.innerText = interviewContainer.children.length
+    // rejectCount.innerText = rejectContainer.children.length
+
+
+    const counts = {
+        all: allContainer.children.length ,
+        interview :interviewContainer.children.length,
+        reject :rejectContainer.children.length
+    };
+    totalCount.innerText = counts.all;
+    interviewCount.innerText= counts.interview;
+    rejectCount.innerText = counts.reject;
+
+    available.innerText = counts[currentStatus];
+
+    if(counts[currentStatus] < 1){
+        emptyState.classList.remove('hidden')
+    }else{
+        emptyState.classList.add('hidden')
+    }
+}
+updateState();
